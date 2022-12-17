1f032cbe8f07bc20d3001c79c9d0493502e6472f
naing aung <naingaung9863@gmail.com>
2022-12-18 00:31:40 +0630

    redesign

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-const getmenuBtn = document.querySelector(".menuBtn");
-const getmenuNav = document.querySelector(".navbar-nav");
-const getheader = document.querySelector("header");
-const getcloseBtn = document.querySelector(".closeBtn");
+let gethamburgetBtn = document.querySelector(".hamburgerBtn");
+let getmenu = document.querySelector(".nav_menu");
+let getlines = document.querySelectorAll(".lines");
 
-let MenuDisplay;
-
-getmenuBtn.addEventListener("click", function () {
-	MenuDisplay = document.createElement("div");
-
-	MenuDisplay.innerHTML = `
-            	
-					<ul class=" navbar-nav d-lg-none text-uppercase navResponsive">
-					    <button type="button" class="closeBtn">x</button>
-						<li class="navbar-item"><a href="#" class="navbar-link">Home</a></li>
-						<li class="navbar-item"><a href="#" class="navbar-link">Our Menus</a></li>
-						<li class="navbar-item"><a href="#" class="navbar-link">About us</a></li>
-						<li class="navbar-item"><a href="#" class="navbar-link">reservation</a></li>
-					</ul>`;
-
-	getheader.appendChild(MenuDisplay);
+gethamburgetBtn.addEventListener("click", function () {
+	for (let i = 0; i < getlines.length; i++) {
+		getlines[i].classList.toggle("active");
+	}
+	getmenu.classList.toggle("active");
 });
